20fecf9f8c3a38e9666908a46c8843a7904af7a6
tweedlemoon <197379865@qq.com>
2021-11-25 21:42:56 +0800

    20211125批量归一化学习完毕

--- Pytorch_Basic/24BatchNormalization.py
+++ Pytorch_Basic/24BatchNormalization.py
@@ -4,6 +4,7 @@ from torch import nn, optim
 import torch.nn.functional as F
 
 import sys
+import os
 
 sys.path.append("..")
 import d2lzh_pytorch as d2l
@@ -11,12 +12,19 @@ import d2lzh_pytorch as d2l
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
+# %%
+
 def batch_norm(is_training, X, gamma, beta, moving_mean, moving_var, eps, momentum):
     # 判断当前模式是训练模式还是预测模式
     if not is_training:
         # 如果是在预测模式下，直接使用传入的移动平均所得的均值和方差
         X_hat = (X - moving_mean) / torch.sqrt(moving_var + eps)
     else:
+        # assert函数意为断言
+        # (2,4)是一个元组，不可改变的那个，意思是如果len(X.shape)是2、4中任意一个，则继续进行，否则报错，报错可以在后面指定，如
+        # assert len(X.shape) in (2, 4), "X.shape is not in any of 2 or 4"
+        # 记得2是全连接层的批量归一化，因为是2维，只有n*d，n数据量，d维度
+        # 4是放于卷积层里的批量归一化，因为是4维，n*c*l*h，n数据，c通道数量，l长，h宽
         assert len(X.shape) in (2, 4)
         if len(X.shape) == 2:
             # 使用全连接层的情况，计算特征维上的均值和方差
@@ -25,9 +33,16 @@ def batch_norm(is_training, X, gamma, beta, moving_mean, moving_var, eps, moment
         else:
             # 使用二维卷积层的情况，计算通道维上（axis=1）的均值和方差。这里我们需要保持
             # X的形状以便后面可以做广播运算
+            # 至于keepdim，敲一下以下代码就知道怎么回事了
+            # a = Tensor([[1,2,3],[3,2,1]])
+            # a.mean(dim=0,keepdim=True).shape
+            # a.mean(dim=0).shape
             mean = X.mean(dim=0, keepdim=True).mean(dim=2, keepdim=True).mean(dim=3, keepdim=True)
+            # 手敲方差公式哈哈
             var = ((X - mean) ** 2).mean(dim=0, keepdim=True).mean(dim=2, keepdim=True).mean(dim=3, keepdim=True)
         # 训练模式下用当前的均值和方差做标准化
+        # 维度要一样不然X - mean可能会出问题
+        # 疑点在这，不带keepdim会出现啥，没觉得有啥
         X_hat = (X - mean) / torch.sqrt(var + eps)
         # 更新移动平均的均值和方差
         moving_mean = momentum * moving_mean + (1.0 - momentum) * mean
@@ -62,6 +77,7 @@ class BatchNorm(nn.Module):
         return Y
 
 
+# 使用的是19LeNet
 net = nn.Sequential(
     nn.Conv2d(1, 6, 5),  # in_channels, out_channels, kernel_size
     BatchNorm(6, num_dims=4),
@@ -82,7 +98,13 @@ net = nn.Sequential(
 )
 
 batch_size = 256
-train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size=batch_size)
+
+# 读数据
+directory = os.path.abspath(os.path.join(os.getcwd(), "../", "Datasets"))
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size=batch_size, root=directory)
 
 lr, num_epochs = 0.001, 5
 optimizer = torch.optim.Adam(net.parameters(), lr=lr)
