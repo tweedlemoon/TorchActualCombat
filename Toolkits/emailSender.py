9c7e8894906772ae5658a3bfd3596cced8bae9dc
tweedlemoon <197379865@qq.com>
2022-01-23 21:10:58 +0800

    20220123程序发送邮件测试成功

--- Toolkits/emailSender.py
+++ Toolkits/emailSender.py
@@ -1,4 +1,7 @@
+import datetime
 import smtplib
+import os
+import sys
 from email.mime.text import MIMEText
 from email.utils import formataddr
 
@@ -7,42 +10,97 @@ from email.utils import formataddr
 EMAIL_ADDRESS = 'e_lyffly@126.com'
 # 发件邮箱密码
 EMAIL_PASSWORD = '197379865'
-# 收件邮箱
-SEND_TO = '197379865@qq.com'
+# 收件邮箱，可以扩充
+RECEIVERS = ['197379865@qq.com', ]
 
 
 class EmailSender:
-    def __init__(self, proName='', logAdd=None, message=''):
+    def __init__(self, proName='', logAdd='', message=''):
+        """
+        邮件发送类
+        :param proName: 调用方法os.path.basename(sys.argv[0])
+        :param logAdd: 日志地址logAdd
+        :param message: 先设置为空，由后面定义
+        """
         # 程序名
         self.proName = proName
         # 日志地址
         self.logAdd = logAdd
         # 发送的信息
         self.message = message
+        # 时间
+        self.time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
         # 上面三个预设
         self.emailSender = EMAIL_ADDRESS
         self.senderPasswd = EMAIL_PASSWORD
-        self.reciever = SEND_TO
+        self.reciever = RECEIVERS
 
-        # 发送邮件的函数
-        self.sendEmail()
+        self.loadMessage()
 
-    def sendEmail(self):
-        my_user = self.reciever  # 收件人邮箱账号，我这边发送给自己
+    def loadMessage(self):
+        """
+        读入信息
+        :return:self.message
+        """
+        with open(self.logAdd, 'r') as f:
+            for line in f.readlines():
+                self.message += line
+
+            f.close()
+        return self.message
+
+    def sendResultEmail(self):
+        """
+        发送结果邮件
+        :return: Ture or False 代表发送成功与失败
+        """
         ret = True
-        try:
-            # 邮件本体
-            msg = MIMEText('这是一封测试邮件', 'plain', 'utf-8')
-            msg['From'] = formataddr(("From：efly的程序", self.emailSender))  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
-            msg['To'] = formataddr(("FK", my_user))  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
-            msg['Subject'] = "这是一封测试邮件"  # 邮件的主题，也可以说是标题
+        for r in self.reciever:
+            try:
+                # 邮件本体
+                self.getCurrentTime()
+                msg = MIMEText('邮件发送时间:' + self.time + '\n' + '运行结果：' + '\n' + self.message, 'plain', 'utf-8')
+                msg['From'] = formataddr(("来自您的项目" + self.proName, self.emailSender))  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
+                msg['To'] = formataddr(("FK", r))  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
+                msg['Subject'] = "您的程序运行完成"  # 邮件的主题，也可以说是标题
 
-            server = smtplib.SMTP_SSL("smtp.126.com", 465)  # 发件人邮箱中的SMTP服务器，端口是25，qq和网易为465
-            server.login(self.emailSender, self.senderPasswd)  # 括号中对应的是发件人邮箱账号、邮箱密码
-            server.sendmail(self.emailSender, [my_user, ], msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
-            server.quit()  # 关闭连接
-        except Exception:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
-            ret = False
-
+                server = smtplib.SMTP_SSL("smtp.126.com", 465)  # 发件人邮箱中的SMTP服务器，端口是25，qq和网易为465
+                server.login(self.emailSender, self.senderPasswd)  # 括号中对应的是发件人邮箱账号、邮箱密码
+                server.sendmail(self.emailSender, r, msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
+                server.quit()  # 关闭连接
+            except Exception:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
+                ret = False
         return ret
+
+    def sendErrorEmail(self):
+        """
+        发送错误邮件
+        :return:
+        """
+        ret = True
+        for r in self.reciever:
+            try:
+                # 邮件本体
+                self.getCurrentTime()
+                msg = MIMEText('邮件发送时间:' + self.time + '\n' + self.message, 'plain', 'utf-8')
+                msg['From'] = formataddr(("来自您的项目" + self.proName, self.emailSender))  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
+                msg['To'] = formataddr(("FK", r))  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
+                msg['Subject'] = "您的程序出现问题"  # 邮件的主题，也可以说是标题
+
+                server = smtplib.SMTP_SSL("smtp.126.com", 465)  # 发件人邮箱中的SMTP服务器，端口是25，qq和网易为465
+                server.login(self.emailSender, self.senderPasswd)  # 括号中对应的是发件人邮箱账号、邮箱密码
+                server.sendmail(self.emailSender, r, msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
+                server.quit()  # 关闭连接
+            except Exception:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
+                ret = False
+        return ret
+        pass
+
+    def getCurrentTime(self):
+        """
+        获取当前时间
+        :return: none
+        """
+        self.time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # 获取当前时间
+        return
--- Toolkits/gpuInfo.py
+++ Toolkits/gpuInfo.py
@@ -1,4 +1,8 @@
+import datetime
 import torch
+import sys
+import os
+from Toolkits.emailSender import EmailSender
 
 # 判定cuda是否可用
 available = torch.cuda.is_available()
@@ -8,11 +12,41 @@ device = torch.device("cuda:0" if (torch.cuda.is_available() and gpu_num > 0) el
 device_name = torch.cuda.get_device_name(device) if (torch.cuda.is_available() and gpu_num > 0) else "cpu"
 
 if __name__ == '__main__':
-    print('cuda是否可用：', available)
-    # print('gpu数量：', gpu_num)
-    print('设备名称：', device_name)
-    # 做一个简单运算
-    if torch.cuda.is_available():
-        print(torch.rand(3, 3).cuda())
-    else:
-        print(torch.rand(3, 3))
+    # 定义日志位置
+    # 文件名是：时间+当前的文件名去掉后缀+_log.txt
+    logName = os.path.splitext(os.path.basename(sys.argv[0]))[0] + '_' + datetime.datetime.now().strftime(
+        '%Y-%m-%d_%H-%M-%S') + '_log.txt'
+    logFilePath = os.getcwd() + '/../LogFiles/' + logName
+    logFile = open(logFilePath, 'w', buffering=1)
+    try:
+        # 输出重定向
+        # 先把原来的控制台保存一下
+        __console__ = sys.stdout
+        __error__ = sys.stderr
+        # 标准输出和标准错误输出在日志文件中
+        sys.stdout = logFile
+        sys.stderr = logFile
+
+        print('cuda是否可用：', available)
+        # print('gpu数量：', gpu_num)
+        print('设备名称：', device_name)
+        # 做一个简单运算
+        if torch.cuda.is_available():
+            result = torch.rand(3, 3).cuda()
+            print(result)
+        else:
+            result = torch.rand(3, 3)
+            print(result)
+        # 再把控制台和标准错误还原回去
+        sys.stdout = __console__
+        sys.stderr = __error__
+        print('控制台已经还原，程序结束！')
+        logFile.close()
+
+        # 发成功邮件
+        sender = EmailSender(proName=os.path.basename(sys.argv[0]), logAdd=logFile.buffer.name)
+        sender.sendResultEmail()
+    except Exception:
+        # 发错误邮件
+        sender = EmailSender(proName=os.path.basename(sys.argv[0]), logAdd=logFile.buffer.name)
+        sender.sendErrorEmail()
